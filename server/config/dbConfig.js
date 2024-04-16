8e014e7391d67bc54fb697c9b23add02402a0ea1
p4rz1v4l26 <20211cst0067@gmail.com>
2024-04-16 14:29:37 +0530

    Mongo server setup

---
+++ server/config/dbConfig.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URL);
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Connected to DB");
+});
+connection.on("error", (err) => {
+  console.log(err);
+});
+
+module.exports = connection;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig.js");
 
 const port = process.env.PORT || 5000;
-app.listen(port,() => {
-    console.log(`Server is running on port  + ${port}`);
-}); 
+app.listen(port, () => {
+  console.log(`Server is running on port  ${port}`);
+});
