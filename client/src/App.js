3b2c0eed66907a88d075610a742ed4a800f29e3c
p4rz1v4l26 <20211cst0067@gmail.com>
2024-04-16 11:11:38 +0530

    working on Login function

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import { Button } from "antd";
+import "./stylesheets/theme.css";
+import "./stylesheets/textelements.css";
+import "./stylesheets/alignments.css";
+import "./stylesheets/custom-components.css";
+import "./stylesheets/form-elements.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/common/login";
+import Register from "./pages/common/register";
+
 function App() {
   return (
-    <div className="App">
-      <h1> QuizGramm</h1>
-      <Button type="primary">Button</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/common/login/index.js
@@ -0,0 +1,41 @@
+import { Form } from 'antd';
+import React from 'react'
+
+function login() {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen bg-white">
+      <div className="card w-400 p-3 bg-white">
+        <div className="flex flex-col">
+          <div className="flex">
+            <h1 className="text-2xl">QuizGramm <i class="ri-login-circle-line"></i></h1>
+            
+          </div>
+          <div className="divider w-100"></div>
+          <Form layout="vertical" className="mt-2" >
+            <Form.Item name="email" label="Email">
+              <input type="text" />
+            </Form.Item>
+            <Form.Item name="password" label="Password">
+              <input type="password" />
+            </Form.Item>
+
+            <div className="flex flex-col gap-2">
+              <button
+                type="submit"
+                className="primary-contained-btn mt-2 w-100"
+              >
+                Login
+              </button>
+              {/* <Link to="/register" className="underline">
+                Not a member? Register
+              </Link> */}
+            </div>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default login
